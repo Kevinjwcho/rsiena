2f65b301ee7d960b8e7666351c8f54eb382033c8
Jinwoo Cho <kevinjwcho@gmail.com>
2025-10-26 16:35:45 -0400

    siena test comment revise

--- tests/sienaDependent_test.r
+++ tests/sienaDependent_test.r
@@ -1,8 +1,7 @@
 # install.packages("devtools")
 library(devtools)
 
-# 패키지 로드
-load_all()   # 현재 디렉토리의 R/ 아래 파일들 불러오기
+load_all()
 
 set.seed(7)
 X <- array(sample(c(0,1,10,11,NA), 8*8*8*3, replace=TRUE, prob=c(.86,.09,.02,.02,.01)),
