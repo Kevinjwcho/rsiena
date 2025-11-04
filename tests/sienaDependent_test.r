1cb035f30e5d755a711fa863b560ff0f72b80eab
Jinwoo Cho <kevinjwcho@gmail.com>
2025-11-04 10:28:53 -0500

    notes

--- tests/sienaDependent_test.r
+++ tests/sienaDependent_test.r
@@ -11,6 +11,21 @@ attr(Y, "type")      # "threeway"
 attr(Y, "netdims")   # 8 8 8 3
 attr(Y, "nodeSet")   # "Actors"
 
-dat = sienaDataCreate(Y)
+dat = sienaDataCreate(Y) # undirected or directed density
+dat$dycCovars
+dat$vCovars
+dat$observations
 print(dat)
 eff <- getEffects(dat)
+print(eff) # density  check the rate initial value 
+# Is the perceived network directed or undirected?
+# Constraints on parameters (are some of them equal)
+# getEffects(dat, homogenous = TRUE) / self-reported case (estimate are different with the perceived)
+# Goodness of fit: outlier(include covariate effects)
+
+# Individual characteristics
+# Dyadic covariates: Distance btw where people live./ perceive more friendship ties for people who live close to you (Dyadic effect (perceiver and sender))
+# Perceiver, sender, receiver effects (Right now I just included sender-receiver effects)
+
+# Next includeEffects(): I think it would be similar to getEffects()
+# sienaAlgorithmCreate() and siena07() would be the most intense work. 
